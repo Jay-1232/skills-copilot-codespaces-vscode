9be22dbf79620433f7f9aadb2bbc3e595b20ab0f
Jay Sahu <114244101+Jay-1232@users.noreply.github.com>
2023-07-28 10:41:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'PHP'];
+    var member = {
+        name: 'Code',
+        age: 20,
+        skills: skills
+    };
+    console.log(member.skills);
+} 
